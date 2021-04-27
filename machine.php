1ca30b59dfe5580df3f69e8e8c7d416bc024fb24
MEGARISSE-Yannick <67727389+MEGARISSE-Yannick@users.noreply.github.com>
2021-04-27 16:46:18 +0400

    Add files via upload
    fichier affichage machines et utilisateurs

---
+++ machine.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MachineCoffee</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="javascript.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+    <!--affichage de tous les pc avec une icone pour les pc utilisé-->
+    <section class="hero is-primary">
+        <div class="container has-text-centered">
+            <img src="https://zupimages.net/up/21/15/9r1f.png" alt="" />
+
+            <p class="title">
+                Toutes les machines
+            </p> <br>
+
+            <p class="subtitle">
+                Modifier une machine ajouter ou annuler une reservation
+            </p>
+
+            <form method="post">
+                <label for="nom_machine"> Machine </label>
+                <br><br>
+                <input class="input" type="text" placeholder="Nom de la machine" name="nom_machine">
+                <br><br>
+                <button type="submit" class="button is-success"><i class="fas fa-rocket"></i>Ajouter</button>
+
+                <?php
+                include('config.php');
+                $reponse = $db->query('SELECT * FROM machine');
+
+                if (isset($_POST['nom_machine'])) {
+                    $requete = 'INSERT INTO machine VALUES(NULL,"disponible","' . $_POST['nom_machine'] . '" )';
+                    $resultat = $db->query($requete);
+                }
+                ?>
+            </form>
+        </div>
+        <br><br><br><br>
+
+        <div class="container has-text-centered">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nom machine</th>
+                        <th>Statut</th>
+                        <th>Modification</th>
+                        <th>Suppression</th>
+                        <th>Annulation</th>
+
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // On inclut la connexion à la base
+                    require_once('config.php');
+
+                    // On écrit notre requête
+                    $sql_machine = 'SELECT * FROM `machine`';
+
+                    // On prépare la requête
+                    $query_machine = $db->prepare($sql_machine);
+
+                    // On exécute la requête
+                    $query_machine->execute();
+
+                    // On stocke le résultat dans un tableau associatif
+                    $result_machine = $query_machine->fetchAll(PDO::FETCH_ASSOC);
+
+                    require_once('close.php');
+
+                    foreach ($result_machine as $machine) {
+                    ?>
+                        <tr>
+                            <td><?= $machine['nom_machine'] ?></td>
+
+
+                            <td><?= $machine['statut'] ?></td>
+                            <td><a class="button is-warning" href="modif-machine.php?id=<?= $machine['id'] ?>">
+                                    <i class="fas fa-user-edit"></i></a></td>
+                            <td><a class="button is-danger" href="supprimer-machine.php?id=<?= $machine['id'] ?>">
+                                    <i class="fas fa-user-times"></i></a></td>
+                            <td><a class="button is-success" href="annulation.php?id=<?= $machine['id'] ?>">
+                                    <i class="fas fa-user-minus"></i></a></td>
+
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+
+        </div>
+
+        </div>
+        <br><br><br>
+
+    </section>
+
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UserCoffee!</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="javascript.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include('navbar.php'); ?>
+
+
+    <section class="hero is-dark is-small">
+        <div class="container has-text-centered">
+            <img src="https://zupimages.net/up/21/15/9r1f.png" alt="" />
+
+            <p class="title">
+                Tous les utilisateurs
+            </p> <br>
+
+            <p class="subtitle">
+                Creer un utilisateur le modifier ou le supprimer
+            </p>
+            <form method="post">
+                <label for="pseudo"> User </label>
+                <br><br>
+                <input class="input" type="text" placeholder="Nom de l'utilisateur" name="pseudo">
+                <br><br>
+                <button type="submit" class="button is-black"><i class="fas fa-rocket"></i>Ajouter</button>
+
+                <?php
+                include('config.php');
+
+                $reponse = $db->query('SELECT * FROM utilisateur');
+                if (isset($_POST['pseudo'])) {
+                    $requete = 'INSERT INTO utilisateur VALUES(NULL, "' . $_POST['pseudo'] . '" )';
+                    $resultat = $db->query($requete);
+                }
+                ?>
+            </form>
+        </div>
+        <br><br><br><br>
+
+        <div class="container has-text-centered">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Pseudo</th>
+                        <th>Modification</th>
+                        <th>Suppression</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+
+                    // On inclut la connexion à la base
+                    require_once('config.php');
+
+                    // On écrit notre requête
+                    $sql = 'SELECT * FROM `utilisateur`';
+
+                    // On prépare la requête
+                    $query_ = $db->prepare($sql);
+
+                    // On exécute la requête
+                    $query_->execute();
+
+                    // On stocke le résultat dans un tableau associatif
+                    $result = $query_->fetchAll(PDO::FETCH_ASSOC);
+
+                    require_once('close.php');
+
+                    foreach ($result as $utilisateur) {
+                    ?>
+                        <t-r>
+                            <td><?= $utilisateur['pseudo'] ?></td>
+                            <td><a class="button is-warning" href="modif-user.php?id=<?= $utilisateur['id'] ?>"><i class="fas fa-user-edit"></i></a></td>
+                            <td><a class="button is-danger" href="supprimer-user.php?id=<?= $utilisateur['id'] ?>"><i class="fas fa-user-times"></i></a></td>
+                            </tr>
+                        <?php
+                    }
+                        ?>
+                        <?php
+
+                        ?>
+
+
+                </tbody>
+            </table>
+
+        </div>
+        <br><br><br>
+    </section>
+
+
+</body>
+
+</html>
